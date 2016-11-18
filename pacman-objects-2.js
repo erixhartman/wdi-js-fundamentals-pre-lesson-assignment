f4e6c480f17fee1199ee11ab7469b296a74fc8d8
Eric Hartman <imerichartman@gmail.com>
2016-11-18 17:06:30 -0500

    assignment complete

---
+++ pacman-objects-2.js
@@ -0,0 +1,6 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'}
+console.log(ghosts);
+console.log(ghosts.inky); // => 'Cyan'
+console.log(ghosts.blinky); // => 'Red'
+console.log(ghosts['inky']); // => 'Cyan'
+console.log(ghosts['blinky']); // => 'Red'
